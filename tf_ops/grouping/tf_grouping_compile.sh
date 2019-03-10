f33e58b4d36903339132ff26819f505b5a0b4508
syncle <syncle@gmail.com>
2019-03-10 01:09:02 -0800

    python -> python3

--- tf_ops/grouping/tf_grouping_compile.sh
+++ tf_ops/grouping/tf_grouping_compile.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 /usr/local/cuda-8.0/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
-TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC \
   -I ${TF_INC} \
